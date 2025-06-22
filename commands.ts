adb1c14dc2b9b5997b2461353838d5b97d88dbbe
cristian-5 <cristianantonuccio@icloud.com>
2025-06-22 17:18:02 +0200

    refactor

--- commands.ts
+++ commands.ts
@@ -1,6 +1,6 @@
 
 import { Ping } from "./commands/ping.ts";
 import { Elo } from "./commands/elo.ts";
-import { FEN } from "./commands/diagram.ts";
+import { FEN } from "./commands/fen.ts";
 
 export const COMMANDS = [ Ping, Elo, FEN ];
