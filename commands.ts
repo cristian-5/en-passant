2bbf2a32c79ab9896ca385d2c20bd857c8c5e56b
cristian-5 <cristianantonuccio@icloud.com>
2025-06-24 16:07:01 +0200

    test pgn 2

--- commands.ts
+++ commands.ts
@@ -2,5 +2,6 @@
 import { Ping } from "./commands/ping.ts";
 import { Elo } from "./commands/elo.ts";
 import { FEN } from "./commands/fen.ts";
+import { PGN } from "./commands/pgn.ts";
 
-export const COMMANDS = [ Ping, Elo, FEN ];
+export const COMMANDS = [ Ping, Elo, FEN, PGN ];
